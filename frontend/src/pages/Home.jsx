5a1d5e156b8969f6fa59b85e1c4d4e76c533d042
abhishek-0512 <abhishekgangwar9876@gmail.com>
2026-07-09 18:28:43 +0530

    Your descriptive commit message

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="text-2xl text-red-500">
+      HomePage
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div className="bg-[#dddbdb] w-[100vw] h-[100vh] flex items-center justify-center">
+      <form className></form>
+      
+    </div>
+  );
+}
+
+export default SignUp;
